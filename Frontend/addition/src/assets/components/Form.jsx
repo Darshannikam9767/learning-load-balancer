fc3c5abc0b272b613fed1fc44fc4005c9c498256
Darshannikam9767 <darshannikam64@gmail.com>
2026-09-18 19:17:36 +0530

    implemented backend

--- Frontend/addition/src/assets/components/Form.jsx
+++ Frontend/addition/src/assets/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios"
 import Result from "./Result";
 
 const Form = () => {
@@ -7,40 +8,65 @@ const Form = () => {
     const [secondNum, setSecondNum] = useState("");
     const [showResult, setShowResult] = useState(false);
     const [sum, setSum] = useState("");
-    const submitHandler = (e)=>{
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log("Form Submitted.....");
-        setSum( Number(firstNum) + Number(secondNum));
-        setShowResult(true);
-        setFirstNum("");
-        setSecondNum("");
-        
-    }
-  return (
-    <>
-        <form onSubmit={submitHandler} className="bg-white w-full max-w-sm md:max-w-md flex flex-col items-center p-5 rounded-3xl border border-gray-300 shadow-2xl shadow-black/60 gap-1" >
-            <h1 className="text-3xl font-bold tracking-wider uppercase text-black/40">addition</h1>
-            <div className="h-px w-full bg-black/70 rounded-full mt-2 mb-5"/>
-            
-            <input onChange={(e)=>{
-                setFirstNum(e.target.value)
-                
-            }}
-            value={firstNum}
-            className=" w-[90%] bg-gray-100 shadow-2xl shadow-black/70  px-4 py-2 outline-1 outline-gray-400 focus:outline-blue-600 border-gray-400 rounded-2xl mb-3 placeholder:text-black/60 placeholder:capitalize font-semibold tracking-wide" type="text" placeholder="enter first number" />
-            <input
-            onChange={(e)=>{
-                setSecondNum(e.target.value)
-            }}
-            value={secondNum}
-            className=" w-[90%] bg-gray-100 shadow-2xl shadow-black/70  px-4 py-2 outline-1 outline-gray-400 focus:outline-blue-600 border-gray-400 rounded-2xl mb-3 placeholder:text-black/60 placeholder:capitalize font-semibold tracking-wide" type="text" placeholder="enter second number" />
-            
-            <button className="bg-black px-4 py-2 text-white uppercase tracking-widest rounded-full font-semibold mt-3 cursor-pointer hover:scale-98 transition-all duration-150 active:scale-102 shadow-xl shadow-black/60">calculate</button>
-        </form>
 
-        {showResult &&(<Result sum={sum} setShowResult={setShowResult} />)}
-    </>
-  )
+        const num1 = firstNum;
+        const num2 = secondNum;
+        if (num1 && num2) {
+
+            try {
+                const { data } = await axios.post("http://localhost:3000/api/calculate", {
+                    num1: num1,
+                    num2: num2
+
+                })
+                setSum(`${num1} + ${num2} = ${data.sum}`)
+
+                setShowResult(true);
+                setFirstNum("");
+                setSecondNum("");
+            } catch (error) {
+                console.log(error);
+
+            }
+        } else {
+            setSum("Both Numbers are mandatory...!")
+            setShowResult(true);
+        }
+
+
+
+    }
+    return (
+        <>
+            <form onSubmit={submitHandler} className="bg-white w-full max-w-sm md:max-w-md flex flex-col items-center p-5 rounded-3xl border border-gray-300 shadow-2xl shadow-black/60 gap-1" >
+                <h1 className="text-3xl font-bold tracking-wider uppercase text-black/40">addition</h1>
+                <div className="h-px w-full bg-black/70 rounded-full mt-2 mb-5" />
+
+                <input onChange={(e) => {
+                    if(showResult)
+                        setShowResult(false)
+                    setFirstNum(e.target.value)
+
+                }}
+                    value={firstNum}
+                    className=" w-[90%] bg-gray-100 shadow-2xl shadow-black/70  px-4 py-2 outline-1 outline-gray-400 focus:outline-blue-600 border-gray-400 rounded-2xl mb-3 placeholder:text-black/60 placeholder:capitalize font-semibold tracking-wide" type="text" placeholder="enter first number" />
+                <input
+                    onChange={(e) => {
+                        if(showResult)
+                            setShowResult(false)
+                        setSecondNum(e.target.value)
+                    }}
+                    value={secondNum}
+                    className=" w-[90%] bg-gray-100 shadow-2xl shadow-black/70  px-4 py-2 outline-1 outline-gray-400 focus:outline-blue-600 border-gray-400 rounded-2xl mb-3 placeholder:text-black/60 placeholder:capitalize font-semibold tracking-wide" type="text" placeholder="enter second number" />
+
+                <button className="bg-black px-4 py-2 text-white uppercase tracking-widest rounded-full font-semibold mt-3 cursor-pointer hover:scale-98 transition-all duration-150 active:scale-102 shadow-xl shadow-black/60">calculate</button>
+            </form>
+
+            {showResult && (<Result sum={sum} setSum={setSum} setShowResult={setShowResult} />)}
+        </>
+    )
 }
 
 export default Form
--- Frontend/addition/src/assets/components/Result.jsx
+++ Frontend/addition/src/assets/components/Result.jsx
@@ -1,9 +1,10 @@
 
-const Result = ({sum,setShowResult}) => {
+const Result = ({sum,setSum,setShowResult}) => {
 
     setTimeout(()=>{
         setShowResult(false);
-    },10000)
+        setSum("")
+    },20000)
   return (
     <div className=" flex items-center justify-center absolute top-5 border-2 border-green-500 rounded-3xl py-5 bg-green-200  w-full max-w-md md:max-w-lg px-5  shadow-xl shadow-black/40">
       <h5 className="text-2xl font-bold text-green-600 tracking-wider">{sum}</h5>
